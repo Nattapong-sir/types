3a2bdee99f6b46192e546516395f3ed31ca70842
Nattapong-sir <33569075+Nattapong-sir@users.noreply.github.com>
2018-04-18 16:50:00 +0700

    Add ResponseSession

--- express-session/index.d.ts
+++ express-session/index.d.ts
@@ -1,4 +1,4 @@
-import {Request, RequestHandler} from 'express'
+import {Request, Response, RequestHandler} from 'express'
 
 declare class Cookie {
     secure?: boolean
@@ -30,6 +30,10 @@ export interface Session extends RequestHandler {
 }
 
 export interface RequestSession extends Request {
+    session: any
+}
+
+export interface ResponseSession extends Response {
     session: any
 }
 
